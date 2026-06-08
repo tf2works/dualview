bb8f353f80bb226d6375be140e02b2e3a0686618
Flaubert TAGU FONKU <109031049+tf2works@users.noreply.github.com>
2026-06-08 23:55:01 +0200

    v0.4.4 - update installer

--- installer/build-installer.bat
+++ installer/build-installer.bat
@@ -1,9 +1,9 @@
 @echo off
 echo.
-echo  DualView v0.4.3 - Build
+echo  DualView v0.4.4 - Build
 echo  ========================
 echo.
-echo  Produit dist\DualView-Setup-0.4.3.exe
+echo  Produit dist\DualView-Setup-0.4.4.exe
 echo  Inclut un desinstallateur Windows natif.
 echo.
 echo  Prerequis : Node.js v22+ (https://nodejs.org)
